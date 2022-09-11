073db6cf9fa74e24b0e91ff9b35f6b6d2a97abc8
Natnael Melese <natrix277@gmail.com>
2022-09-11 00:13:31 -0400

    Some unittests

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -6,8 +6,21 @@ import os
 sys.path.append(os.path.abspath(os.path.join("../Pharmaceutical-Sales-Prediction-across-multiple-stores/")))
 
 from scripts import cleaning_pipeline
+df = pd.read_csv("./tests/train_sample_data.csv")
 
 class TestDataClean(unittest.TestCase):
     def setUp(self) -> None:
         # self.df = df_g.copy()
         self.cleaner = cleaning_pipeline.CleaningPipeline()
+
+    def test_convert_to_datetime(self):
+        self.cleaner.convert_to_datetime(df, ['Date'])
+        self.assertEqual(df['Date'].dtype, "datetime64[ns]")
+    
+    def test_convert_to_string(self):
+        self.cleaner.convert_to_string(df, ['StateHoliday'])
+        self.assertEqual(df['StateHoliday'].dtype, "string")
+
+
+if __name__ == "__main__":
+    unittest.main()
